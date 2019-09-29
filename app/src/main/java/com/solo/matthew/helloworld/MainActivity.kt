5bd9632296f4821c041ee376adcf2c148e692110
Matthew Williams <matthew.williams@impact.com>
2019-09-30 00:02:48 +0200

    Introduce image view with dice image for each dice roll

--- app/src/main/java/com/solo/matthew/helloworld/MainActivity.kt
+++ app/src/main/java/com/solo/matthew/helloworld/MainActivity.kt
@@ -1,24 +1,36 @@
 package com.solo.matthew.helloworld
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
-        val resultText: TextView = findViewById(R.id.result_text)
+        diceImage = findViewById(R.id.dice_image)
 
         rollButton.setOnClickListener {
             val randomInt: Int = Random().nextInt(6) + 1
-            resultText.text = randomInt.toString()
+            diceImage.setImageResource(diceDrawable(randomInt))
+        }
+    }
+
+    private val diceDrawable = { randomInt: Int ->
+        when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
     }
 }
